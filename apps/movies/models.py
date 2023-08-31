d0ba7397eaf3ba47c9354c75fdff558b3d6dddaf
Silvestre525 <silvestrevillar02@gmail.com>
2023-08-31 17:51:15 -0300

    inicializacion de Proyecto

---
+++ apps/movies/models.py
@@ -0,0 +1,12 @@
+# movies/models.py
+
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    release_date = models.DateField()
+    director = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
